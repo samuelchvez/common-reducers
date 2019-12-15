66b3a3ffc41cf30ac1667f071579a64146e8b65d
Samuel Chávez <samuelchvez@gmail.com>
2019-12-15 12:36:03 -0600

    Next page

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import error from './error';
 import errors  from './errors ';
 import fetching from './fetching';
 import isFetching from './isFetching';
+import nextPage from './nextPage';
 import order from './order';
 import orderById from './orderById';
 import selected from './selected';
@@ -23,6 +24,7 @@ export {
   errors,
   fetching,
   isFetching,
+  nextPage,
   order,
   orderById,
   selected,
