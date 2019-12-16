c1afe487efa24a05046c7ab411a18ed11986c409
Samuel Chávez <samuelchvez@gmail.com>
2019-12-16 17:09:08 -0600

    New customizable order key

--- orderById.js
+++ orderById.js
@@ -6,49 +6,56 @@ type OrderByIdConfigurationType = {
   fetched?: Array<string>,
   replaced?: Array<string>,
   idKey?: string,
+  orderKey?: string,
 };
 
 type OrderByIdActionType = {
   type: string,
   payload: {
-    order: Array<ID_TYPE>
-  }
+    order?: Array<ID_TYPE>
+  },
 };
 
 const orderById = (configuration: OrderByIdConfigurationType) => (
   state: {[ID_TYPE]: Array<ID_TYPE>} = {},
-  action: OrderByIdActionType
+  action: OrderByIdActionType,
 ): {[ID_TYPE]: Array<ID_TYPE>} => {
   const {
     fetched,
     replaced,
     idKey = 'id',
+    orderKey = 'order',
   } = configuration;
 
   const { payload } = action;
 
   if(fetched != null && fetched.includes(action.type)) {
-    const { order } = payload;
-    const objectId = payload[idKey];
-    const originalOrder = state[objectId] || [];
-    const stateSet = new Set(originalOrder);
-    const difference = order.filter(
-       id => !stateSet.has(id)
-    );
+    let order = payload[orderKey];
+    if (order && order.constructor === Array) {
+      const objectId = payload[idKey];
+      const originalOrder = state[objectId] || [];
+      const stateSet = new Set(originalOrder);
+      const difference = order.filter(
+        id => !stateSet.has(id)
+      );
 
-    return {
-      ...state,
-      [objectId]: [ ...originalOrder, ...difference ]
-    };
+      return {
+        ...state,
+        [objectId]: [ ...originalOrder, ...difference ],
+      };
+    }
   }
 
   if(replaced != null && replaced.includes(action.type)) {
-    const { order } = payload;
-    const objectId = payload[idKey];
-    return {
-      ...state,
-      [objectId]: order
-    };
+    const order = payload[orderKey];
+
+    if (order && order.constructor === Array) {
+      const objectId = payload[idKey];
+      return {
+        ...state,
+        [objectId]: order,
+      };
+    }
   }
 
   return state;
