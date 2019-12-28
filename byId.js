c800d81bd638707205264fed791b2db07d494bef
Samuel Chávez <samuelchvez@gmail.com>
2019-12-28 01:19:10 -0600

    NaN Edge case

--- byId.js
+++ byId.js
@@ -143,7 +143,7 @@ const byId = (configuration: ByIdConfigurationType) => (
           newEntities[id] = {
             ...(defaultAttributes || {}),
             ...(payload.entities || {})[
-              Number.isNaN(id) ? id : parseInt(id, 10)
+              Number.isNaN(parseInt(id)) ? id : parseInt(id, 10)
             ],
             isConfirmed: true,
           };
